8733214364a109e10d6c873e68abfb606fd301b5
Suppakron <suwannapop01@gmail.com>
2023-11-16 20:39:39 +0700

    Lab1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner number = new Scanner(System.in);
+//        System.out.println("-------------------");
+//        System.out.println("Input first number");
+        int firstNumber = number.nextInt();
+//        System.out.println("-------------------");
+        if (checkFirstNumber(firstNumber)){
+//            System.out.println("Input");
+            int[] arr = new int[firstNumber];
+            for (int i = 0; i < firstNumber; i++) {
+                arr[i] = number.nextInt();
+            }
+//            System.out.println("-------------------");
+            Arrays.sort(arr);
+//            System.out.println("-------------------");
+            for ( int x: arr) {
+                System.out.println(x);
+            }
+//            System.out.println("-------------------");
+        }else {
+            System.out.println("Number should more than 0");
         }
+    }
+    public  static  boolean checkFirstNumber(int x){
+        if (x >= 0){
+            return true;
+        }
+        return  false;
     }
 }
