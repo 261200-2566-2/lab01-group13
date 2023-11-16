aff5b4dbb7476e64868fea282212c671d5f7c214
Suppakron <suwannapop01@gmail.com>
2023-11-16 20:45:42 +0700

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
-//        System.out.println("-------------------");
-//        System.out.println("Input first number");
         int firstNumber = number.nextInt();
-//        System.out.println("-------------------");
         if (checkFirstNumber(firstNumber)){
-//            System.out.println("Input");
             int[] arr = new int[firstNumber];
             for (int i = 0; i < firstNumber; i++) {
                 arr[i] = number.nextInt();
             }
-//            System.out.println("-------------------");
             Arrays.sort(arr);
-//            System.out.println("-------------------");
             for ( int x: arr) {
                 System.out.println(x);
             }
-//            System.out.println("-------------------");
         }else {
             System.out.println("Number should more than 0");
         }
@@ -27,7 +20,6 @@ public class Main {
     public  static  boolean checkFirstNumber(int x){
         if (x >= 0){
             return true;
-        }
-        return  false;
+        }else {return false; }
     }
 }
